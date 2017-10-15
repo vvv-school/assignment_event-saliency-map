846569fcce20f65c07a2b4ec75e08c6bc1f074e9
Ugo Pattacini <ugo.pattacini@iit.it>
2017-10-15 20:01:01 +0200

    Update test.cpp

--- smoke-test/test.cpp
+++ smoke-test/test.cpp
@@ -95,23 +95,23 @@ public:
         //we need to load the data file into yarpdataplayer
         std::string cntlportname = "/playercontroller/rpc";
 
-        RTF_ASSERT_ERROR_IF(playercontroller.open(cntlportname),
-                            "Could not open RPC to yarpdataplayer");
+        RTF_ASSERT_ERROR_IF_FALSE(playercontroller.open(cntlportname),
+                                  "Could not open RPC to yarpdataplayer");
 
-        RTF_ASSERT_ERROR_IF(yarp::os::Network::connect(cntlportname, "/yarpdataplayer/rpc:i"),
-                            "Could not connect RPC to yarpdataplayer");
+        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect(cntlportname, "/yarpdataplayer/rpc:i"),
+                                  "Could not connect RPC to yarpdataplayer");
 
 //        //we need to check the output of yarpdataplayer is open and input of spiking model
-//        RTF_ASSERT_ERROR_IF(yarp::os::Network::connect("/zynqGrabber/vBottle:o", "/vOrientationFilter/vBottle:i", "udp"),
-//                            "Could not connect yarpdataplayer to spiking model");
+//        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/zynqGrabber/vBottle:o", "/vOrientationFilter/vBottle:i", "udp"),
+//                                  "Could not connect yarpdataplayer to spiking model");
 
         //check we can open our spike checking consumer
-        RTF_ASSERT_ERROR_IF(spkchk.open("/spikechecker/vBottle:i"),
-                            "Could not open spike checker");
+        RTF_ASSERT_ERROR_IF_FALSE(spkchk.open("/spikechecker/vBottle:i"),
+                                  "Could not open spike checker");
 
         //the output of spiking model
-        RTF_ASSERT_ERROR_IF(yarp::os::Network::connect("/vSalMap/vBottle:o", "/spikechecker/vBottle:i", "udp"),
-                            "Could not connect saliency map to spike checker");
+        RTF_ASSERT_ERROR_IF_FALSE(yarp::os::Network::connect("/vSalMap/vBottle:o", "/spikechecker/vBottle:i", "udp"),
+                                  "Could not connect saliency map to spike checker");
 
 //        RTF_TEST_REPORT("Ports successfully open and connected");
 
@@ -135,21 +135,21 @@ public:
         yarp::os::Bottle cmd, reply;
         cmd.addString("play");
         playercontroller.write(cmd, reply);
-        RTF_ASSERT_ERROR_IF(reply.get(0).asString() == "ok", "Did not successfully play the dataset");
+        RTF_ASSERT_ERROR_IF_FALSE(reply.get(0).asString() == "ok", "Did not successfully play the dataset");
 
         yarp::os::Time::delay(5);
 
         cmd.clear();
         cmd.addString("stop");
         playercontroller.write(cmd, reply);
-        RTF_ASSERT_ERROR_IF(reply.get(0).asString() == "ok", "Did not successfully stop the dataset");
+        RTF_ASSERT_ERROR_IF_FALSE(reply.get(0).asString() == "ok", "Did not successfully stop the dataset");
 
         int inliers = spkchk.getInliers();
         int outliers = spkchk.getOutliers();
         RTF_TEST_REPORT(Asserter::format("Inliers = %d", inliers));
         RTF_TEST_REPORT(Asserter::format("Outliers = %d", outliers));
-        RTF_ASSERT_ERROR_IF(inliers > 7500, "Inlier score too low (7500)");
-        RTF_ASSERT_ERROR_IF(outliers < 1000, "Outlier score too high (1000)");
+        RTF_ASSERT_ERROR_IF_FALSE(inliers > 7500, "Inlier score too low (7500)");
+        RTF_ASSERT_ERROR_IF_FALSE(outliers < 1000, "Outlier score too high (1000)");
 
 
     }
